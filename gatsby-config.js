0fa6d86f0d22122d2589f5986da70533862e06e1
Owen Craston <owen.craston@shopify.com>
2021-09-29 15:04:17 -0400

    new icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
         background_color: `#000`,
         theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/assets/img/portfolio-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/img/apple-touch-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
