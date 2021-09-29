4e723a743a2cc52672c16e426cb7eae29dd2a57a
Owen Craston <owengc12@gmail.com>
2021-09-29 14:18:11 -0400

    get ready to publish (#6)
    - moved to the root of the repo
    - cleanup queries

---
+++ src/data/data.js
@@ -0,0 +1,58 @@
+module.exports = {
+  SiteTitle: 'Owen Craston',
+  Sitelogo: '#',
+  SiteLogoText: 'Owen Craston',
+  SiteAuthor: 'Owen Craston',
+  SiteDescription: 'Developer',
+  defaultDescription: 'Developer', 
+  githubApiQuery: `query ($number_of_repos: Int!) {
+    viewer {
+      name
+      avatarUrl
+      isHireable
+      resourcePath
+      repositories(last: $number_of_repos, privacy: PUBLIC, orderBy: { field: STARGAZERS, direction:ASC } ) {
+        nodes {
+          name
+          description
+          homepageUrl
+          forkCount
+          createdAt
+          updatedAt
+          resourcePath
+          languages(last: 1, orderBy: { field: SIZE, direction:ASC } ) {
+            edges {
+              node {
+                name
+                color
+              }
+            }
+          }
+          licenseInfo {
+            name
+          }
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }`,
+  githubApiVariables: {
+    number_of_repos: 4,
+  }, 
+  SiteSocialLinks: {
+    github: 'https://github.com/owencraston',
+    linkedin: 'https://www.linkedin.com/in/owencraston/',
+  },
+  SiteAddress: {
+    city: 'Toronto',
+    region: 'Ontario',
+    country: 'Canada',
+    zipCode: 'ZipCode',
+  },
+  SiteContact: {
+    email: 'owengc12@gmail.com',
+  },
+  SiteCopyright: '2021',
+};
