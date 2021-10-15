cec232ba60a3c4d409af16b551268a58d1770160
Owen Craston <owen.craston@shopify.com>
2021-10-15 12:33:28 -0400

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,12 +61,30 @@ module.exports = {
         icon: `src/assets/img/apple-touch-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
+    // https://www.gatsbyjs.com/plugins/gatsby-plugin-google-gtag/
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-          trackingId: process.env.GATSBY_GOOGLE_ANALYTICS,
-          head: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GATSBY_GOOGLE_ANALYTICS, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
